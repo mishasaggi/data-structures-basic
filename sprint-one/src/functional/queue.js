c35c09c1132aa7e4a9d35b94e857caf07e29b1f6
“mishasaggi” <monisha.saggi@gmail.com>
2015-07-29 20:23:07 -0500

    Started queue functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,33 @@ var Queue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
+  var pointer = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[size] = value;
+    console.log("Size before increment: " + size);
+    size++;
+    console.log("Size after increment: " + size);
+
   };
 
   someInstance.dequeue = function(){
+    size && size--;
+    var result = storage[pointer];
+    delete storage[pointer];
+    console.log("Pointer before increment: " + pointer);
+    pointer++;
+    console.log("Pointer after increment: " + pointer);
+    console.log("Result after dequeue: " + result);
+
+    return result;
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
