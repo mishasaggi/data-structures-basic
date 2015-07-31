f823ccd2f8730595cf9ef59042ac192a96573ba1
“mishasaggi” <monisha.saggi@gmail.com>
2015-07-30 20:01:32 -0500

    Wrote comments for linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,6 +4,13 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    // 1. create new Node instance
+    // 2. set new Node value to "value" and set new Node next to "null"
+    // 3. use "tail" to select the last node, and change its "next" to the new Node
+    // 4. set "tail" equal to the new node
+
+
+
   };
 
   list.removeHead = function(){
