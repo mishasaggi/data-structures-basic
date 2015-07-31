0e8700159d4fda32f7d01037a275189ab9f02d22
“mishasaggi” <monisha.saggi@gmail.com>
2015-07-31 11:46:29 -0500

    Completed Set and answered time complexity for Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,25 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = undefined; //don't need it?
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this[item] = item;
+
 };
 
 setPrototype.contains = function(item){
+  return (!!this[item]);
 };
 
 setPrototype.remove = function(item){
+  delete this[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+ // all functions O(1)
