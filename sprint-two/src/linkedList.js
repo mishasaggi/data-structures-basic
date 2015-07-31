e37f11992d2df2bd4e567cd547357a018b0a1bf1
“mishasaggi” <monisha.saggi@gmail.com>
2015-07-31 10:01:53 -0500

    Completed LinkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -6,17 +6,41 @@ var LinkedList = function(){
   list.addToTail = function(value){
     // 1. create new Node instance
     // 2. set new Node value to "value" and set new Node next to "null"
-    // 3. use "tail" to select the last node, and change its "next" to the new Node
-    // 4. set "tail" equal to the new node
-
-
-
+    // 3. use "tail" to select the last node, and evaluate its "next" to the new Node
+    // 4. set "tail" point to the new node
+    if(list.tail === null){
+      list.tail = Node(value);
+      list.head = list.tail;
+    }
+    else{
+      list.tail.next = Node(value);
+      list.tail = list.tail.next;
+    }
   };
 
   list.removeHead = function(){
+    var result = list.head.value;
+    list.head && (list.head = list.head.next);
+    return result;
   };
 
   list.contains = function(target){
+
+    var checkNode = function(currentNode){
+      if (currentNode.value === target) {
+        return true;
+      }
+      else if (currentNode.next === null){
+        return false;
+      }
+      else{
+        return checkNode(currentNode.next);
+      }
+
+    }
+
+    return checkNode(list.head);
+
   };
 
   return list;
