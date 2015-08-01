9e974a5291bf9e7f918bcb1a6fde1580a6b832b1
“mishasaggi” <monisha.saggi@gmail.com>
2015-07-31 20:04:26 -0500

    Started binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,44 @@
 var BinarySearchTree = function(value){
+  var newBinarySearchTree = {};
+
+  newBinarySearchTree.left = null;
+  newBinarySearchTree.right = null;
+  newBinarySearchTree.value = value;
+
+  _.extend(newBinarySearchTree, binarySearchTreeMethods);
+
+  return newBinarySearchTree;
 };
 
+var binarySearchTreeMethods = {};
+
+binarySearchTreeMethods.insert = function(value) {
+  var newNode = BinarySearchTree(value);
+  if (value < this.value) {
+    if (this.left) {
+      this.left.insert(newNode);
+    }
+    else {
+      this.left = newNode;
+    }
+  } else {
+    if (this.right) {
+      this.right.insert(newNode);
+    }
+    else {
+      this.right = newNode;
+    }
+
+  }
+};
+
+binarySearchTreeMethods.contains = function(value) {
+
+};
+
+binarySearchTreeMethods.depthFirstLog = function(cb) {
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
